9173c85bccfe698962c06f3c8640e347c9137606
Selena <149027636+possiblyselena@users.noreply.github.com>
2026-06-01 16:08:01 -0400

    hover dropdown

--- app/components/documents.tsx
+++ app/components/documents.tsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 type ImportantItem = {
   id: string;
   label: string;
+  text: string;
+  image: string;
   checked: boolean;
 };
 
 const initialItems: ImportantItem[] = [
-  { id: "watcard", label: "WatCard (or whatever your school's ID is called)", checked: false },
-  { id: "healthcard", label: "Health Card", checked: false },
-  { id: "bankcard", label: "Bank Card", checked: false },
-  { id: "driverslicense", label: "Driver's License", checked: false },
-  { id: "passport", label: "Passport", checked: false },
+  { id: "watcard", label: "WatCard (or whatever your school's ID is called)", text:"Hello", image: "", checked: false },
+  { id: "healthcard", label: "Health Card", text: "", image: "", checked: false },
+  { id: "bankcard", label: "Bank Card", text: "", image: "", checked: false },
+  { id: "driverslicense", label: "Driver's License", text: "", image: "", checked: false },
+  { id: "passport", label: "Passport", text: "Not required, but you never know where you're going to go. ", image: "important/passport.svg", checked: false },
 ];
 
 export function Important() {
@@ -34,16 +36,24 @@ export function Important() {
 
         <ul className="bg-white p-2 rounded-lg space-y-2">
           {items.map((item) => (
-            <li key={item.id} className="rounded-lg">
+            <li key={item.id} className="group rounded-lg">
               <label className="flex items-center gap-2">
-                <input
-                  type="checkbox"
-                  checked={item.checked}
-                  onChange={() => toggleChecked(item.id)}
-                />
-                <span className={item.checked ? "line-through" : undefined}>
-                  {item.label}
-                </span>
+                <div className="display-grid grid-rows-2 gap-1">
+                  <div className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      checked={item.checked}
+                      onChange={() => toggleChecked(item.id)}
+                    />
+                    <span className={item.checked ? "line-through" : undefined}>
+                      {item.label}
+                    </span>
+                  </div>
+                  <div id="details" className="hidden group-hover:block">
+                    {item.image && <img src={item.image} alt={item.label} className="w-6 h-6" />}
+                    {item.text && <p className="text-sm text-gray-600">{item.text}</p>}
+                  </div>
+                </div>
               </label>
             </li>
           ))}
