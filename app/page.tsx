b338f5425a380d61b37cb8e92f0f66854d6a4034
Selena <149027636+possiblyselena@users.noreply.github.com>
2026-06-01 13:48:49 -0400

    fixed the draggable element

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,23 @@ import { Skincare } from "./components/skincare";
 import { Stationary } from "./components/supplies";
 import { Washroom } from "./components/washroom";
 import Draggable from "react-draggable";
+import { useRef } from "react";
 
 export default function Home() {
+  const nodeRef = useRef(null);
   return (
     <div>
       <h1>Selena's Moving Out Packing List</h1>
       <p>A glorified to-do list app.  </p>
-      <Draggable>
-        <Important/>
+      <Draggable nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <Important/>
+        </div>
       </Draggable>
-      <Draggable>
-        <p>Hallo</p>
+      <Draggable nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <p>Hallo</p>
+        </div>
       </Draggable>
     </div>
   );
